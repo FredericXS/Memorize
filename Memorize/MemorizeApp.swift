af59d974c23309c71ed5ff0c210844e0a6ff3f1e
FredericXS <xsfrederic@gmail.com>
2023-12-30 03:23:54 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Frederic 石英  on 30/12/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
